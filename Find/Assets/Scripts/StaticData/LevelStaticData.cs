a27b734e10640a96839838935a58753cb3850216
Евгений <listopadov_94@mail.ru>
2024-04-22 20:45:33 +0300

    add

---
+++ Find/Assets/Scripts/StaticData/LevelStaticData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Scripts.StaticData
+{
+    [CreateAssetMenu(fileName ="LevelData",menuName =("StaticData/Level"))]
+    public class LevelStaticData : ScriptableObject
+    {
+        public int NumberLeve;
+        public GameObject Prefab;
+    }
+}
---
+++ Find/Assets/Scripts/StaticData/StaticDataService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Scripts.StaticData
+{
+    public class StaticDataService : IStaticDataService
+    {
+        private Dictionary<int, LevelStaticData> _level;
+
+        public void LoadLevel()
+        {
+            _level = Resources.LoadAll<LevelStaticData>("StaticDataLevel")
+                .ToDictionary(x => x.NumberLeve, x => x);
+        }
+
+        public LevelStaticData ForLevel(int level) =>
+            _level.TryGetValue(level, out LevelStaticData levelStaticData) ? levelStaticData : null;
+    }
+}
